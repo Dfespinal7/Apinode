791a8d6c69a1f1e44612bfeebbd4c3bb302b908f
Dfespinal7 <dfespinal7@misena.edu.co>
2024-04-02 21:39:34 -0500

    se crearon nuevas rutas para el crud

--- index.js
+++ index.js
@@ -7,12 +7,62 @@ app.use(exp.json())
 
 let modelProducto=require('./backend/models/products.model')
 
-app.get('/listarproducto',(req,res)=>{
+app.get('/productos',async(req,res)=>{
+    let listarproductos=await modelProducto.find();
+    if(listarproductos)
+        res.status(200).json(listarproductos)
+    else
+        res.status(404).json({error:"no se encontraron productos"})
     
 })
+
+app.get('/productos/:ref',async(req,res)=>{
+    let productoEncontrado=await modelProducto.findOne({referencia:req.params.ref});
+    if(productoEncontrado)
+        res.status(200).json(productoEncontrado)
+    else
+        res.status(400).json({"error":"producto no encontrado"})
+    
+})
+
+app.post('/productos',async(req,res)=>{
+    const nuevoProducto={
+        referencia:req.body.referenciaProducto,
+        nombre:req.body.nombreProducto,
+        descripcion:req.body.descripcionProducto,
+        precio:req.body.precioProducto,
+        stock:req.body.stockProducto,
+        imagen:req.body.imagenProducto,
+        habilitado:true
+    }
+    let insercion=await modelProducto.create(nuevoProducto);
+    if(insercion)
+        res.status(200).json({"mensaje":"registro exitoso"})
+    else
+        res.status(404).json({"mensaje":"se presentó un error"})
+})
+
+
+app.put('/productos:ref',async(req,res)=>{
+    const productoEditado={
+        referencia:req.params.ref,
+        nombre:req.body.nombreProducto,
+        descripcion:req.body.descripcionProducto,
+        precio:req.body.precioProducto,
+        stock:req.body.stockProducto,
+        imagen:req.body.imagenProducto,
+        habilitado:true
+    }
+    let actualizacion=await modelProducto.findOneAndUpdate({referencia:req.params.ref},productoEditado)
+    if(actualizacion)
+        res.status(200).json({"mensaje":"actualizacion exitoso"})
+    else
+        res.status(404).json({"mensaje":"se presentó un error"})
+})
+
 
 app.listen(process.env.PORT,()=>{
     console.log("servidor en linea")
 })
 
-"prueba de push"
+
